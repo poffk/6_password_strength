07a4ac5ba7dabc7ab3c88ee1e7cecc1710d2404b
Alexander <Alexander>
2016-11-29 19:32:33 +0300

    1.Пользователь указывает путь до черного списка 2.Функции проверки и оценки разделены

--- password_strength.py
+++ password_strength.py
@@ -7,7 +7,7 @@ def load_data(filepath):
         return data_file.read()
 
 
-def password_length_scoring(password):
+def password_length_check_and_rate(password):
     if len(password) < 4:
         return 1
     elif len(password) < 12:
@@ -16,46 +16,58 @@ def password_length_scoring(password):
         return 3
 
 
-def password_register_scoring(password):
-    if re.findall(r'[A-Z]', password) and re.findall(r'[a-z]', password):
-        return 2
-    else:
-        return 0
+def password_register_check(password):
+    return re.findall(r'[A-Z]', password) and re.findall(r'[a-z]', password)
 
 
-def password_figures_and_letters_scoring(password):
-    if re.findall(r'[^ \W|\d]', password) and re.findall(r'[\d]', password):
-        return 2
-    else:
-        return 0
+def password_register_rate(password, password_strength_mark):
+    if password_register_check(password):
+        password_strength_mark += 2
+    return password_strength_mark
 
 
-def password_special_letters_scoring(password):
-    if re.findall(r'\W', password):
-        return 2
-    else:
-        return 0
+def password_figures_and_letters_check(password):
+    return re.findall(r'[^ \W|\d]', password) and re.findall(r'[\d]', password)
 
 
-def password_blacklist_scoring(password, blacklist):
-    if password not in re.findall(r'\w+', blacklist):
-        return 1
-    else:
-        return 0
+def password_figures_and_letters_rate(password, password_strength_mark):
+    if password_figures_and_letters_check(password):
+        password_strength_mark += 2
+    return password_strength_mark
+
+
+def password_special_letters_check(password):
+    return re.findall(r'\W', password)
+
+
+def password_special_letters_rate(password, password_strength_mark):
+    if password_special_letters_check(password):
+        password_strength_mark += 2
+    return password_strength_mark
+
+
+def password_blacklist_check(password, blacklist):
+    return password not in re.findall(r'\w+', blacklist)
+
+
+def password_blacklist_rate(password, blacklist, password_strength_mark):
+    if password_blacklist_check(password, blacklist):
+        password_strength_mark += 1
+    return password_strength_mark
 
 
 def get_password_strength(password, blacklist):
     password_strength_mark = 0
-    password_strength_mark += password_length_scoring(password)
-    password_strength_mark += password_register_scoring(password)
-    password_strength_mark += password_figures_and_letters_scoring(password)
-    password_strength_mark += password_special_letters_scoring(password)
-    password_strength_mark += password_blacklist_scoring(password, blacklist)
+    password_strength_mark = password_length_check_and_rate(password)
+    password_strength_mark = password_register_rate(password, password_strength_mark)
+    password_strength_mark = password_figures_and_letters_rate(password, password_strength_mark)
+    password_strength_mark = password_special_letters_rate(password, password_strength_mark)
+    password_strength_mark = password_blacklist_rate(password, blacklist, password_strength_mark)
     return password_strength_mark
 
 
 if __name__ == '__main__':
     print('Введите пароль для проверки: ')
     password = getpass.getpass()
-    blacklist = load_data('blacklist.txt')
-    print(get_password_strength(password, blacklist))
+    blacklist = load_data(input('Введите путь до черного списка паролей: '))
+    print('Оценка Вашего пароля: {}'.format(get_password_strength(password, blacklist)))
